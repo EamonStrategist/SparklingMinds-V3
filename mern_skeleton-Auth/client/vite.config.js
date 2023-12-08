132f2226f555f0ddc50b5cfd086f07b8b0dfbc64
EamonStrategist <jhutch24@my.centennialcollege.ca>
2023-12-07 20:51:20 -0500

    updated vite.config.js

--- mern_skeleton-Auth/client/vite.config.js
+++ mern_skeleton-Auth/client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })*/
-
+/*
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
@@ -16,5 +16,35 @@ export default defineConfig({
     rollupOptions: {
       input: "./src/main.jsx",
     },
+  },
+});
+import dotenv from 'dotenv';
+dotenv.config();
+*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const { PORT = 3000 } = process.env;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+      '/auth': {
+        target: `http://localhost:${PORT}`,
+        changeOrigin: true,
+      },
+     
+    },
+  },
+  build: {
+     outDir: '../dist/app',
   },
 });
